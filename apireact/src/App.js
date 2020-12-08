5dd7f837303ceed1cf19f163f9c20048c09359d0
bhautik koladiya <bhautik211298@gmail.com>
2020-12-08 15:00:05 +0530

    ok

--- apireact/src/App.js
+++ apireact/src/App.js
@@ -1,8 +1,18 @@
 import React,{useState,useEffect} from 'react';
-import {Row, Col,Table,Button,Popconfirm} from 'antd';
 import axios from "axios";
+import {Row, Col,Button,Popconfirm,Card,Form,Input} from 'antd';
+import {UserOutlined} from "@ant-design/icons";
+import Table from "antd/lib/table";
+let EditId = null;
 
 const App = () => {
+
+    const [userDetail, setUserDetail] = useState({
+        employee_name: "",
+        employee_salary: "",
+        employee_age: "",
+        id: ""
+    });
 
     const [data, setData] = useState([]);
 
@@ -10,7 +20,35 @@ const App = () => {
         initial();
     },[])
 
-    const initial = () =>{
+    const handleChange = e => {
+        const {name, value} = e.target;
+        setUserDetail({...userDetail, [name]: value})
+    }
+
+    const onsubmit = () => {
+        if (EditId !== null) {
+            axios.put(`http://dummy.restapiexample.com/api/v1/update/${EditId}`, userDetail)
+                .then(res => {
+                    setUserDetail(data[EditId])
+                    console.log(res)
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        } else {
+            userDetail.id = data.length + 1;
+            axios.post(`http://dummy.restapiexample.com/api/v1/create`)
+                .then(res => {
+                    setData(res.data.data)
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        }
+        setUserDetail({})
+    }
+
+    const initial = () => {
         axios.get('http://dummy.restapiexample.com/api/v1/employees')
             .then(res => {
                 setData(res.data.data)
@@ -20,13 +58,27 @@ const App = () => {
             })
     }
 
-    const T = 'Are you sure to delete this task?';
+    const onEdit = (id) => {
+            EditId =id;
+            const findUserDetail = data.find(user => user.id === (id))
+            setUserDetail(findUserDetail)
 
-    const onEdit = (id, data) => {
-        return axios.put(`http://dummy.restapiexample.com/api/v1/update/${id}`, data)
-            .then(res => {
-                console.log()
-            })
+        // return axios.get(`http://dummy.restapiexample.com/api/v1/employee/${id}`,data)
+        //     .then(res => {
+        //         console.log(res)
+        //     })
+        //     .catch(err => {
+        //         console.log(err)
+        //     })
+        // return axios.put(`http://dummy.restapiexample.com/api/v1/update/${id}`, data)
+        //     .then(res => {
+        //         setUserDetail(data[id])
+        //         console.log(res)
+        //     })
+        //     .catch(err => {
+        //         console.log(err)
+        //     })
+        // initial();
     }
 
     const onDelete = id => {
@@ -37,9 +89,10 @@ const App = () => {
             .catch(err => {
                 console.log(err)
             })
-        initial();
+        // initial();
     }
 
+    const T = 'Are you sure to delete this task?';
     const columns = [
         {
             title: 'ID',
@@ -70,11 +123,11 @@ const App = () => {
             dataIndex: 'id',
             render: (text, record) => (
                 <>
-                    <Button type="primary" onClick={() => onEdit(record)}>
+                    <Button type="primary" onClick={() => onEdit(record.id)}>
                         Edit
                     </Button>
                     &nbsp;&nbsp;
-                    <Popconfirm placement="right" title={T} onConfirm={() => onDelete(record)} okText="Yes"
+                    <Popconfirm placement="right" title={T} onConfirm={() => onDelete(data)} okText="Yes"
                                 cancelText="No">
                         <Button type="primary" danger>
                             Delete
@@ -90,8 +143,40 @@ const App = () => {
   return (
       <>
           <Row style={{marginTop: 100}}>
-              <Col span={4}/>
-              <Col span={16} className="mt-3">
+              <Col span={7}/>
+              <Col span={10}>
+              <Card>
+                <Form>
+                    <Form.Item>
+                        <Input placeholder="Enter Employee_Name" name="employee_name"
+                               value={userDetail.employee_name}
+                               onChange={handleChange} addonBefore={(<UserOutlined/>)}/>
+                        {/*<span className="error">{errors.employee_name}</span>*/}
+                    </Form.Item>
+                    <Form.Item>
+                        <Input placeholder="Enter Employee_salary" name="employee_salary"
+                               value={userDetail.employee_salary}
+                               onChange={handleChange} addonBefore={(<UserOutlined/>)}/>
+                        {/*<span className="error">{errors.Employee_salary}</span>*/}
+                    </Form.Item>
+                    <Form.Item>
+                        <Input placeholder="Enter Employee_Age" name="employee_age"
+                               value={userDetail.employee_age}
+                               onChange={handleChange} addonBefore={(<UserOutlined/>)}/>
+                        {/*<span className="error">{errors.Employee_Age}</span>*/}
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit" onClick={onsubmit}>
+                            Submit
+                        </Button>
+                    </Form.Item>
+                </Form>
+              </Card>
+              </Col>
+          </Row><br />
+          <Row >
+              <Col span={7}/>
+              <Col span={10} className="mt-3">
                   <Table
                       columns={columns}
                       dataSource={data || []}
